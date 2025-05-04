6ac01f3a883ed5c981ee670012295451db068313
Aditya <adityashaw82005@gmail.com>
2025-05-04 20:12:26 +0530

    Fixed spacing

--- src/components/todos.js
+++ src/components/todos.js
@@ -5,7 +5,7 @@ function Todos(props) {
     return (
         <div className="m-4">
             <h3>"TO DO LIST"</h3>
-            <div className="m-3">
+            <div className="m-4">
                 {
                     props.todos.map(todo => (
                        <Todoitem title={todo.title} desc={todo.desc} todos={props.todos} settodos={props.settodos}/>
