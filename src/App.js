e39f70da96fd588d51883bb8ca1b1474888f6482
Aditya <adityashaw82005@gmail.com>
2025-05-06 22:37:32 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-
-import './App.css';
-import Navbar from './components/navbar';
-import Todos from './components/todos';
-import Addtodo from './components/addtodo';
-import { useState } from 'react';
-
-
-
+import "./App.css";
+import Navbar from "./components/navbar";
+import Todos from "./components/todos";
+import Addtodo from "./components/addtodo";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./components/about";
 
 function App() {
-   const [todos, settodos] = useState([]);
+  const [todos, settodos] = useState([]);
 
   return (
     <>
-    <Navbar title="To Dos"/>
-    <Addtodo todos={todos} settodos={settodos}/>
-    <Todos todos={todos} settodos={settodos}/>
+      <Navbar title="To Dos" />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Addtodo todos={todos} settodos={settodos} />
+              <Todos todos={todos} settodos={settodos} />
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <p className='fs-2'>print about</p>
+    </div>
+  )
+}
+
+export default About
